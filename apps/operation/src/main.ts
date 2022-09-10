66cc5aa6aab78f8e5ae344a82415dc4840c55ff9
peterismic@gmail.com <peterismic@gmail.com>
2022-09-09 22:22:07 -0400

    feat: testing feat
    BREAKING CHANGE: no decs

--- apps/operation/src/main.ts
+++ apps/operation/src/main.ts
@@ -3,16 +3,18 @@
  * This is only a minimal backend to get started.
  */
 
-import * as express from 'express';
+import express from 'express'
+import { data } from '@rfiready/data'
 
-const app = express();
+console.log(data())
+
+const app = express()
 
 app.get('/api', (req, res) => {
-  res.send({ message: 'Welcome to operation!' });
-});
-
-const port = process.env.port || 3333;
+  res.send({ message: 'Welcome to operation!' })
+})
+const port = 3333
 const server = app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}/api`);
-});
-server.on('error', console.error);
+  console.log(`Listening at http://localhost:${port}/api`)
+})
+server.on('error', console.error)
